d03993cf6b9ac7237265b446a6eb93149065c10b
aljensen92 <aljensen92@gmail.com>
2023-09-16 18:26:03 -0500

    Adjust scatter plot, add filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,110 @@
 import React, { useEffect, useState } from 'react';
-import ReactApexChart from 'react-apexcharts'; 
-
+import ReactApexChart from 'react-apexcharts';
 
 function App() {
-    const [data, setData] = useState([]);
-  
-    useEffect(() => {
-      fetchDataAndPopulateTable();
-    }, []);
-  
-    async function fetchDataAndPopulateTable() {
-      try {
-        const response = await fetch('http://localhost:3000/fetchData'); 
-        const data = await response.json();
-        setData(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [askingPriceRange, setAskingPriceRange] = useState({ min: 0, max: Infinity });
+  const [scatterData, setScatterData] = useState({
+    series: [
+      {
+        name: 'Scatter Plot',
+        data: [],
+      },
+    ],
+  });
+  const [scatterOptions, setScatterOptions] = useState({
+    chart: {
+      height: "100%",
+      type: 'scatter',
+      zoom: {
+        enabled: true,
+        type: 'xy',
+      },
+    },
+    xaxis: {
+      title: {
+        text: 'TTM REVENUE',
+      },
+    },
+    yaxis: {
+      title: {
+        text: 'ASKING PRICE',
+      },
+    },
+    tooltip: {
+      custom: function ({ series, seriesIndex, dataPointIndex }) {
+        return `<div>${filteredData[seriesIndex][dataPointIndex].title}</div>`;
+      },
+    },
+  });
+
+  useEffect(() => {
+    fetchDataAndPopulateTable();
+  }, []);
+
+  async function fetchDataAndPopulateTable() {
+    try {
+      const response = await fetch('http://localhost:4000/fetchData');
+      const responseData = await response.json();
+      setData(responseData);
+      setFilteredData(responseData);
+      populateScatterData(responseData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
     }
-  
-    
-    const scatterData = {
+  }
+
+  function populateScatterData(dataToUse) {
+    const scatterDataArray = dataToUse.map((row) => ({
+      x: parseFloat(row['TTM REVENUE']),
+      y: parseFloat(row['ASKING PRICE']),
+      title: row['Title'],
+    }));
+    setScatterData({
       series: [
         {
           name: 'Scatter Plot',
-          data: data.map((row) => ({
-            x: parseFloat(row['TTM REVENUE']),
-            y: parseFloat(row['ASKING PRICE']),
-            title: row['Title'],
-          })),
+          data: scatterDataArray,
         },
       ],
-    };
-  
-    
-    const scatterOptions = {
-      chart: {
-        height: 350000000,
-        type: 'scatter',
-        zoom: {
-          enabled: true,
-          type: 'xy',
-        },
-      },
-      xaxis: {
-        title: {
-          text: 'TTM REVENUE', // X-axis label
-        },
-      },
-      yaxis: {
-        title: {
-          text: 'ASKING PRICE', // Y-axis label
-        },
-      },
-      tooltip: {
-        custom: function ({ series, seriesIndex, dataPointIndex }) {
-          return `<div>${data[seriesIndex][dataPointIndex].title}</div>`; // Display title in tooltip
-        },
-      },
-    };
-  
-    return (
-      <div className="App">
-        <h1>MiDA</h1>
-        <div id="chart">
-          <ReactApexChart options={scatterOptions} series={scatterData.series} type="scatter" height={350000000} />
-        </div>
-      </div>
-    );
+    });
   }
-  
-  export default App;
-  
+
+  function applyFilter() {
+    const newData = data.filter(d => {
+      const askingPrice = parseFloat(d['ASKING PRICE']);
+      return askingPrice >= askingPriceRange.min && askingPrice <= askingPriceRange.max;
+    });
+    setFilteredData(newData);
+    populateScatterData(newData);
+  }
+
+  return (
+    <div className="App">
+      <h1>MiDA</h1>
+      
+      <div>
+        <input 
+          type="number" 
+          placeholder="Min Asking Price" 
+          value={askingPriceRange.min} 
+          onChange={(e) => setAskingPriceRange(prev => ({ ...prev, min: parseFloat(e.target.value) }))} 
+        />
+        <input 
+          type="number" 
+          placeholder="Max Asking Price" 
+          value={askingPriceRange.max} 
+          onChange={(e) => setAskingPriceRange(prev => ({ ...prev, max: parseFloat(e.target.value) }))} 
+        />
+        <button onClick={applyFilter}>Apply Filter</button>
+      </div>
+      
+      <div id="chart">
+        <ReactApexChart options={scatterOptions} series={scatterData.series} type="scatter" height={550} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
